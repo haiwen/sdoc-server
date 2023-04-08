589bc6d56fd69688105947f8fc6605790e7cd02d
杨顺强 <978987373@qq.com>
2023-04-08 18:10:00 +0800

    add reconnect event handle

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,5 @@
+// base api prefix
 export const BASE_URL_VERSION1 = '/api/v1/files';
+
+// cached operations limit
+export const OPERATIONS_CACHE_LIMIT = 1000;
---
+++ src/managers/operations-manager.js
@@ -0,0 +1,43 @@
+import { OPERATIONS_CACHE_LIMIT } from '../constants';
+
+class OperationsManager {
+
+  constructor() {
+    this.instance = null;
+    this.filesMap = new Map();
+  }
+
+  static getInstance = () => {
+    if (!this.instance) {
+      this.instance = new OperationsManager();
+    }
+
+    return this.instance;
+  };
+
+  addOperations = (fileUUid, operations) => {
+    let operationList = this.filesMap.get(fileUUid) || [];
+    operationList.push(operations);
+    if (operationList.length >= OPERATIONS_CACHE_LIMIT) {
+      operationList = operationList.slice(100);
+    }
+    this.filesMap.set(fileUUid, operationList);
+  };
+
+  getLoseOperationList = (fileUuid, version) => {
+    const operationList = this.filesMap.get(fileUuid);
+    return operationList.filter(item => item.version >= version);
+  };
+
+  getOperationsSize = (fileUuid) => {
+    const operationList = this.filesMap.get(fileUuid);
+    return operationList.length;
+  };
+
+  clearOperations = (fileUuid) => {
+    this.filesMap.delete(fileUuid);
+  };
+
+}
+
+export default OperationsManager;
