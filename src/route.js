00b19fc9f0cb5eec7f2cb4903298b56ead9e6a15
杨顺强 <978987373@qq.com>
2023-04-21 10:07:49 +0800

    add api

--- src/route.js
+++ src/route.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import multipart from 'connect-multiparty';
 import { BASE_URL_VERSION1 } from './constants';
-import { documentController } from './controllers';
+import { documentController, userController } from './controllers';
 
 const router = express.Router();
 const multipartMiddleware = multipart();
 
 router.get(`${BASE_URL_VERSION1}/:doc_uuid/`, documentController.loadDocContent);
 router.post(`${BASE_URL_VERSION1}/:doc_uuid/`, multipartMiddleware, documentController.saveDocContent);
+
+router.get(`${BASE_URL_VERSION1}/:doc_uuid/collaborators`, userController.getCollaborators);
 
 export default router;
