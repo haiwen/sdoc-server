e423b23d72d72fce7c3ffbbb20a70c335a7a08b6
skywalker <dzmbbs@qq.com>
2023-06-08 17:35:08 +0800

    renameSdocHistory

--- sql/sdoc.sql
+++ sql/sdoc.sql
@@ -10,3 +10,13 @@ CREATE TABLE `operation_log` (
   KEY `operation_log_doc_uuid` (`doc_uuid`),
   KEY `idx_operation_log_doc_uuid_op_id` (`doc_uuid`,`op_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `history_name` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `doc_uuid` varchar(36) NOT NULL,
+  `obj_id` varchar(40) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `history_name_doc_uuid` (`doc_uuid`),
+  UNIQUE KEY `history_name_doc_uuid_obj_id` (`doc_uuid`, `obj_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
