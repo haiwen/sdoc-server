aadb7cf118d4ac53eafcb7d2670266ecb9a7dd49
杨顺强 <978987373@qq.com>
2023-11-04 20:19:33 +0800

    optimize code

--- src/managers/document-manager.js
+++ src/managers/document-manager.js
@@ -173,23 +173,23 @@ class DocumentManager {
     // Get save info
     const { version, format_version, children, docName, last_modify_user = '' } = document;
     const docContent = { version, format_version, children, last_modify_user };
-    
+
     let saveFlag = false;
     const tempPath = `/tmp/` + v4();
     fs.writeFileSync(tempPath, JSON.stringify(docContent), { flag: 'w+' });
     try {
       await seaServerAPI.saveDocContent(docUuid, {path: tempPath}, docContent.last_modify_user);
       saveFlag = true;
-      logger.info(`${savedBySocket ? 'Socket: ': ''}${docName}(${docUuid}) saved`);
+      logger.info(`${savedBySocket ? 'Socket: ' : ''}${docName}(${docUuid}) saved`);
     } catch(err) {
       saveFlag = false;
       const message = getErrorMessage(err);
       if (message.status && message.status === 404) {
-        logger.info(`${savedBySocket ? 'Socket: ': ''}${docName}(${docUuid}) failed`);
+        logger.info(`${savedBySocket ? 'Socket: ' : ''}${docName}(${docUuid}) failed`);
         logger.info(JSON.stringify(message));
         await this.removeDoc(docUuid);
       } else {
-        logger.error(`${savedBySocket ? 'Socket: ': ''}${docName}(${docUuid}) failed`);
+        logger.error(`${savedBySocket ? 'Socket: ' : ''}${docName}(${docUuid}) failed`);
         logger.error(JSON.stringify(message));
       }
     } finally {
