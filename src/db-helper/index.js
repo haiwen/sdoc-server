810e7ebca370d49c16fe187a91763fe6d46437a2
skywalker <dzmbbs@qq.com>
2025-05-14 18:33:40 +0800

    dameng

--- src/db-helper/index.js
+++ src/db-helper/index.js
@@ -1,7 +1,7 @@
-import mysql from 'mysql';
+import dmdb from 'dmdb';
 import { MYSQL_CONFIG } from '../config/config';
 
-const mysql_config = {
+const db_config = {
   host: MYSQL_CONFIG.host,
   user: MYSQL_CONFIG.user,
   password: MYSQL_CONFIG.password,
@@ -12,24 +12,45 @@ const mysql_config = {
   timezone: '+00:00'
 };
 
-const pool = mysql.createPool(mysql_config);
+const { user, password, host, port } = db_config;
+const connect_string = `dm://${user}:${password}@${host}:${port}?autoCommit=false&loginEncrypt=false`;
+
+let pool, connection;
+
+async function createPool() {
+  try {
+    return await dmdb.createPool({
+      connectString: connect_string,
+      poolMax: 10,
+      poolMin: 1
+    });
+  } catch (err) {
+    throw new Error("createPool error: " + err.message);
+  }
+}
+
+async function getConnection() {
+  try {
+    return await pool.getConnection();
+  } catch (err) {
+    throw new Error("getConnection error: " + err.message);
+  }
+}
 
 const DBHelper = function (sql, values) {
-  return new Promise((resolve, reject) => {
-    pool.getConnection(function (err, connection) {
-      if (err) {
-        reject(err);
-      } else {
-        connection.query(sql, values, (err, rows) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(rows);
-          }
-          connection.release();
-        });
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!pool) {
+        pool = await createPool();
       }
-    });
+      connection = await getConnection();
+      let result = await connection.execute(sql, values);
+      await connection.commit();
+      await connection.release();
+      resolve(result)
+    } catch (err) {
+      reject(err);
+    }
   });
 };
 
